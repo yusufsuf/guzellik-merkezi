232cb25ab1e0398452c6b370b7bb248e8a1e4b8e
GuzellikMerkezi <dev@guzellik.com>
2026-03-02 03:13:44 +0300

    varsayilan-tema-rengi

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import config from './config'
 
-// Dinamik sayfa başlığı ve tema rengi
+// Dinamik sayfa başlığı
 document.title = `${config.businessName} | ${config.businessSubtitle}`
-const metaTheme = document.querySelector('meta[name="theme-color"]')
-if (metaTheme) metaTheme.content = config.themeColor
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
